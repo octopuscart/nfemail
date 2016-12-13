cbbd76effe6ffdc975c6d6515fd928c8535fe96f
ranirathore <rani21rathore@gmail.com>
2016-12-13 21:39:09 +0530

    Changes in offer menu

---
+++ views/pages_offers.php
@@ -0,0 +1,62 @@
+<?php
+include 'header.php';
+?>
+<!--page title-->
+<section class="page_title_2 bg_light_2 t_align_c relative wrapper" style="    padding: 0px 1px 8px 1px;background: black;">
+    <div class="">
+
+        <!-- breadcrumbs -->
+        <ul class="hr_list d_inline_m breadcrumbs" style="margin-top: 10px;">
+            <li class="m_right_8 f_xs_none" style="margin-right:0px !important">
+                <a href="#" class="color_default d_inline_m m_right_10" style="margin-right:0px !important;color:white;">
+                    <i class="ion-information-circled"></i>&nbsp;&nbsp;  Offers&nbsp;&nbsp;
+                </a>
+            </li>
+
+        </ul>
+    </div>
+</section>
+<style>
+    .page_block p.fw_light.first_letter_1 {
+        font-weight: 500;
+    }
+    .page_block{
+        background-image: url(../assets/images/offers.jpg);
+        background-repeat: no-repeat;
+        background-size: cover;
+        color: #fff;
+        padding: 33px;
+        height: 885px;
+        background-position: -30px -55px;
+
+    }
+    .page_block h3{
+        font-size: 24px;
+        font-weight: 500;
+        color: #FFF;
+    }
+    span.fl.color_pink.circle.t_align_c.f_left.d_block {
+        color: #fff;
+    }
+</style>
+<div class="section_offset" style="    padding: 30px 0 67px;">
+    <div class="container clearfix">
+        <div class="row">
+            <section class="col-lg-12 col-md-12 col-sm-12 m_xs_bottom_30">
+                <h3 class="color_dark fw_light m_bottom_15 heading_1 t_align_c">Our Offers</h3>
+                <p class="m_bottom_35 heading_2 t_align_c"></p>		
+
+            </section>
+
+            <div class="col-lg-12 col-md-12 col-sm-12 m_bottom_40 m_xs_bottom_30 " style='  padding:0 125px;'>
+                <div class="page_block"></div>
+
+            </div>
+        </div>
+    </div>
+</div>
+&nbsp;
+
+<?php
+include 'footer.php';
+?>
