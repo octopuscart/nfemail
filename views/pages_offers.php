8005f86d4cfe44abf4c8babd64675f252474d15d
ranirathore <rani21rathore@gmail.com>
2016-12-13 21:49:33 +0530

    change

--- views/pages_offers.php
+++ views/pages_offers.php
@@ -21,7 +21,7 @@ include 'header.php';
         font-weight: 500;
     }
     .page_block{
-        background-image: url(../assets/images/offers.jpg);
+        background-image: url(../assets/images/page_offers.jpg);
         background-repeat: no-repeat;
         background-size: cover;
         color: #fff;
