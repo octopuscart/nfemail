463f932815e246980c6af25fab0a6156f7828500
ranirathore <rani21rathore@gmail.com>
2017-02-02 20:50:55 +0530

    changes in offer page

--- views/pages_offers.php
+++ views/pages_offers.php
@@ -53,9 +53,13 @@ include 'header.php';
             <div class="col-lg-12 col-md-12 col-sm-12 m_bottom_40 m_xs_bottom_30 " style='  padding:0 125px;'>
                 <div class="page_block">
                     <center>
-                        <a href="http://nitafashions.com/frontend/views/product_list.php?category=0&amp;item_type=1" target="_blank">
-                        <img src="../assets/images/page_offers.jpg" style="    margin-bottom: 10px;
-                             float: left;"></a>
+                        <img src="../assets/nfmail/page_offersn1.jpg" style="   
+                             float: left;">
+                               <a href="http://nitafashions.com/frontend/views/product_list.php?category=0&amp;item_type=1" target="_blank">
+                            <img src="../assets/nfmail/page_offersn2.jpg" style="   
+                             float: left;    margin: -1px 0px;"></a>
+                             <img src="../assets/nfmail/page_offersn3.jpg" style="   
+                             float: left;">
                     </center>
                 </div>
 
